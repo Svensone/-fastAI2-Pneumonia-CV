5aa207146ecbaec6a01e593163c7841822e063ec
svenson.e <svenson.ertel@gmail.com>
2021-04-09 06:11:16 +0800

    clean code

--- app.py
+++ app.py
@@ -3,7 +3,6 @@
 from fastai.basics import *
 from fastai.vision.all import *
 import torchvision.transforms as T
-
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -20,49 +19,12 @@ import base64
 from PIL import Image
 import PIL.Image
 
-
-
 ## get local css
 ##################
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 local_css("style.css")
-
-# adjustment for different systems (share.io PosixPath)
-################################
-# Option 1: when working on localhost:8501
-# temp = pathlib.PosixPath
-# pathlib.PosixPath = pathlib.WindowsPath
-
-# option 2: for when deploying on share.streamlit.io
-plt = platform.system()
-if plt == 'Linux': 
-    pathlib.WindowsPath = pathlib.PosixPath
-else:
-    temp = pathlib.PosixPath
-    pathlib.PosixPath = pathlib.WindowsPath
-# pathlib.PosixPath = temp
-
-## Layout App
-##################
-
-st.title('Pneumonia lung x-ray - Classification')
-st.markdown("""
-## AI - Computer Vision Recognition with **fastai/pytorch**
-
-#### Classifing lung scans if pneumonia or healthy/ normal 
-Dataset: 5.000+ Images
-2021.03.21: Accuracy on ResNet34 Architectur: 67%
-""")
-link1 = 'Model & Data Preprocessing [Github](https://github.com/Svensone/-fastAI2-Pneumonia-CV/blob/master/2021_03_29_%5BfastaiV2%5D_CV_mulitDatasets_Pneumonia_Kaggle.ipynb)'
-link2 = 'Deployment [Github](https://github.com/Svensone/-fastAI2-Pneumonia-CV/tree/master)'
-st.markdown(link1, unsafe_allow_html=True, )
-st.markdown(link2, unsafe_allow_html=True)
-
-st.markdown("""
-## Test the Model yourself
-""")
 
 # Set Background Image *local file"
 ###################################################
@@ -85,13 +47,45 @@ def set_png_as_page_bg(png_file):
         
         st.markdown(page_bg_img, unsafe_allow_html=True)
         return
-
 set_png_as_page_bg('assets/bg2.jpg')
+# adjustment for different systems (share.io PosixPath)
+################################
+# Option 1: when working on localhost:8501
+# temp = pathlib.PosixPath
+# pathlib.PosixPath = pathlib.WindowsPath
 
+# option 2: for when deploying on share.streamlit.io
+plt = platform.system()
+if plt == 'Linux': 
+    pathlib.WindowsPath = pathlib.PosixPath
+else:
+    temp = pathlib.PosixPath
+    pathlib.PosixPath = pathlib.WindowsPath
+# pathlib.PosixPath = temp
+
+## Layout App
+##################
+    ### Header
+    ##############
+st.title('Pneumonia lung x-ray - Classification')
+st.markdown("""
+## AI- Computer Vision with **fastai/pytorch**
+#### Classifing lung scans if pneumonia or healthy 
+**Dataset**: 5.000+ Images of lung x-rays, **Accuracy** on ResNet34 Architectur: 67%
+""")
+link1 = 'Model & Data Preprocessing [Github](https://github.com/Svensone/-fastAI2-Pneumonia-CV/blob/master/2021_03_29_%5BfastaiV2%5D_CV_mulitDatasets_Pneumonia_Kaggle.ipynb)'
+link2 = 'Deployment [Github](https://github.com/Svensone/-fastAI2-Pneumonia-CV/tree/master)'
+st.markdown(link1, unsafe_allow_html=True)
+st.markdown(link2, unsafe_allow_html=True)
+
+    ### Test
+    ##############
+st.markdown("""
+## Test the Model yourself
+""")
 #######################################
 ### Image Classification
 #######################################
-
 def prediction(img, display_img):
     # display the image
     st.image(display_img, use_column_width=True)
@@ -108,19 +102,16 @@ def prediction(img, display_img):
     # Prediction on Image
     predict_class = learn.predict(img)[0]
     predict_prop = learn.predict(img)[2] 
-
-    print(learn.predict(img))
-
     # Display results
     if str(predict_class) == 'NORAML':
         st.success('This is a scan of a healthy lung')
+        st.write()
     else:
         st.success('This scan shows a pneumonia infection')
 
 #######################################
 ### Image Selection
 #######################################
-
 option1= 'Choose a test image from list'
 option2= 'Predict your own Image'
 
@@ -133,9 +124,7 @@ if option == option1:
         'Please select an image:', list_test_img)
     # Read the image
     test_img = test_img
-
     file_path = 'test_images/'+ test_img
-
     img = PILImage.create(file_path)
     # print(img)
     ##### TEST
@@ -157,13 +146,14 @@ else:
             response = requests.get(url)
             pil_img = PIL.Image.open(BytesIO(response.content))
             display_img = np.asarray(pil_img) # Image to display
-            
             # Transform the image
             timg = TensorImage(image2tensor(pil_img))
             tpil = PILImage.create(timg)
             print(tpil)
-
             # call predict func
             prediction(tpil, display_img)
         except:
             st.text("Invalid URL")
+
+link3 = '[Return to Portfolio](https://portfolio-sven.netlify.app/)'
+st.markdown(link3, unsafe_allow_html=True)
